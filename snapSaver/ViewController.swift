b3fdd11645c9191c7df9c4a3f61b106a698d7507
Eric Torigian <eric@toriigan.com>
2016-05-12 09:07:18 -0400

    Initial Commit

---
+++ snapSaver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snapSaver
+//
+//  Created by Eric Torigian on 5/12/16.
+//  Copyright © 2016 Eric Torigian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
